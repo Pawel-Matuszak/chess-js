cd5d7191352d9800ced5de12d4b31c126eb744b9
pawelmatuszak <pawelxmatuszak@gmail.com>
2021-11-16 14:01:34 +0100

    queen class

--- src/components/Queen.js
+++ src/components/Queen.js
@@ -1,4 +1,5 @@
 import Piece from "./Piece";
+import checkDiagonalAndStraigt from "./checkDiagonalAndStraigt";
 
 class Queen extends Piece{
   constructor(type, posX, posY, isWhite){
@@ -7,7 +8,14 @@ class Queen extends Piece{
 
   getLegalMoves(board){
     let legalMoves = [];
-
+    checkDiagonalAndStraigt(1, 0, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(-1, 0, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(0, 1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(0, -1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(1, 1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(-1, 1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(1, -1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
+    checkDiagonalAndStraigt(-1, -1, board, legalMoves, this.pos.x, this.pos.y, this.isWhite)
     return legalMoves;
   }
 }
